a5443714bfd6dd0c6ef703aabc3f25cc167310d4
David Jo <war2290@gmail.com>
2024-04-23 12:50:32 +0900

    init

---
+++ android/app/src/main/kotlin/com/pathorder/pathorder_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pathorder.pathorder_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'pass_order',
+      debugShowCheckedModeBanner: false,
+      // home: MainHolder(),
+      // theme: theme(),
+    );
+  }
+}
