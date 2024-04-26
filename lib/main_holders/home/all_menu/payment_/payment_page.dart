4a7412da2bd5d4ddf132dc55b77423b38a0cd8d3
Pakchan <1625787@donga.ac.kr>
2024-04-26 17:35:46 +0900

    design : order_quest, docs : NestedScrollView -> ListView

--- lib/main_holders/home/all_menu/payment_/payment_page.dart
+++ lib/main_holders/home/all_menu/payment_/payment_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'components/add_menu_button.dart';
+import 'components/order_quest.dart';
 import 'components/payment_bottom_app_bar.dart';
 import 'components/payment_page_app_bar.dart';
 import 'components/pick_up_time.dart';
@@ -14,23 +15,18 @@ class PaymentPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PaymentPageAppBar(),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, _) {
-          return [
-            SliverList(
-              delegate: SliverChildListDelegate([
-                StoreLocation(),
-              ]),
-            ),
-          ];
-        },
-        body: Column(
-          children: [
-            ProductInfo(),
-            AddMenuButton(),
-            PickUpTime(),
-          ],
-        ),
+      body: ListView(
+        children: [
+          StoreLocation(),
+          Column(
+            children: [
+              ProductInfo(),
+              AddMenuButton(),
+              PickUpTime(),
+              OrderQuest(),
+            ],
+          )
+        ],
       ),
       bottomNavigationBar: PaymentBottomAppBar(),
     );
